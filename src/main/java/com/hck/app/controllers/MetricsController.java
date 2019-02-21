f6acbb8976b10d1289828c743cd809a82d70566a
unknown <pablojacobo1998@gmail.com>
2019-02-20 23:40:23 -0600

    Created Method metrics/controller

--- src/main/java/com/hck/app/controllers/MetricsController.java
+++ src/main/java/com/hck/app/controllers/MetricsController.java
@@ -3,13 +3,19 @@ package com.hck.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hck.app.models.entity.Metrics;
 import com.hck.app.models.services.MServiceImpl;
 
+@CrossOrigin(origins = {"http://localhost:4200"}) //CorsConfig
 @RestController
 @RequestMapping("/api/v1")
 public class MetricsController {
@@ -21,4 +27,13 @@ public class MetricsController {
 	public List<Metrics> index(){
 		return metricsService.findAll();
 	}
+	
+	
+	//Saving metrics
+	@PostMapping("/metrics")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Metrics addMetrics(@RequestBody Metrics metrics  ) {
+		return metricsService.saveMetrics(metrics);
+	}
+	
 }

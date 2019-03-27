57b2941688aff78c1f41a020d709942608aaec88
unknown <pablojacobo1998@gmail.com>
2019-03-26 23:34:23 -0600

    fix

--- src/main/java/com/hck/app/auth/ResourceServerConfig.java
+++ src/main/java/com/hck/app/auth/ResourceServerConfig.java
@@ -14,7 +14,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET, "/api/v1/metrics").permitAll()
+			.antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
 			.anyRequest().authenticated();
 	}
 }

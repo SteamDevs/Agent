64be02044a343cf408eb63d60c2829b03f4a999d
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-04-09 23:23:29 -0600

    Add Basic-Keys

--- src/main/java/com/hck/app/auth/JwtConfig.java
+++ src/main/java/com/hck/app/auth/JwtConfig.java
@@ -3,6 +3,9 @@ package com.hck.app.auth;
 public class JwtConfig {
 	public static final String LLAVE_SECRETA = "alguna.clave.secreta.12345678";
 	
+	public static final String BACKEND_APP = "hck_123";
+	public static final String BACKEND_MSG = "...loading..";
+	
 
 
 }

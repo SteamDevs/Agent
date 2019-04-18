973138f5782784d8a809e942ff3b520c1bf26dce
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-04-17 22:07:19 -0600

    key lodash implements

--- src/main/java/com/hck/app/auth/JwtConfig.java
+++ src/main/java/com/hck/app/auth/JwtConfig.java
@@ -6,6 +6,9 @@ public class JwtConfig {
 	public static final String BACKEND_APP = "hck_123";
 	public static final String BACKEND_MSG = "...loading..";
 	
+	//API-PREFIX
+	public static final String API_URL_PREFIX_EXT = "api/v1/lodash";
+	
 
 
 }
